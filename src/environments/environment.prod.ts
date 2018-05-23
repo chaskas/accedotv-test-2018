3ab9f9937a47c0d1bac907e7ea0fb5395b0cdaae
Rodrigo Campos <chaskas@iMac-de-Rodrigo.local>
2018-05-22 23:57:50 -0400

    environment updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBIyK4FcpdJo5oUATa6ckdfUpf0OH29REk',
+    authDomain: 'accedo-test-2018.firebaseapp.com',
+    databaseURL: 'https://accedo-test-2018.firebaseio.com',
+    projectId: 'accedo-test-2018',
+    storageBucket: 'accedo-test-2018.appspot.com',
+    messagingSenderId: '664590778484'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyBIyK4FcpdJo5oUATa6ckdfUpf0OH29REk",
-    authDomain: "accedo-test-2018.firebaseapp.com",
-    databaseURL: "https://accedo-test-2018.firebaseio.com",
-    projectId: "accedo-test-2018",
-    storageBucket: "accedo-test-2018.appspot.com",
-    messagingSenderId: "664590778484"
+    apiKey: 'AIzaSyBIyK4FcpdJo5oUATa6ckdfUpf0OH29REk',
+    authDomain: 'accedo-test-2018.firebaseapp.com',
+    databaseURL: 'https://accedo-test-2018.firebaseio.com',
+    projectId: 'accedo-test-2018',
+    storageBucket: 'accedo-test-2018.appspot.com',
+    messagingSenderId: '664590778484'
   }
 };
 
